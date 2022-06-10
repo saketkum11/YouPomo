39cde84b7cd9509abc24277854b8a04627b91912
saketkum11 <kumarsaket601@gmail.com>
2022-06-10 20:45:04 +0530

    added: js

--- src/index.js
+++ src/index.js
@@ -4,16 +4,13 @@ import "./index.css";
 import App from "./App";
 import { makeServer } from "./server";
 import { BrowserRouter } from "react-router-dom";
-import { TaskProvider } from "./Context/Task-Context/Task-Context";
 
 // Call make Server
 makeServer();
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <TaskProvider>
-        <App />
-      </TaskProvider>
+      <App />
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
